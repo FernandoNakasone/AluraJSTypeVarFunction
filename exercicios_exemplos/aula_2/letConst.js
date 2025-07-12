490ead8c58e932e9513640131899cfe78045bfa6
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-12 19:29:32 -0300

    Exemplos

---
+++ exercicios_exemplos/aula_2/letConst.js
@@ -0,0 +1,13 @@
+let variavel = 10;
+const constante = 20;
+
+const nome = 'Alice';
+console.log(`Olá, meu nome é ${nome}.`);
+
+if (1 > 0) {
+  let nome = 'Ana';
+  console.log(nome); // ‘Ana’
+}
+
+// variável `nome` não está acessível
+console.log(nome); // Error: nome is not defined
