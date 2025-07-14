5e72d8bc98e572e73c2ff5f897583b7b59c70d0e
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-13 22:41:52 -0300

    Exercicio chuva

---
+++ exercicios_exemplos/aula_2/chuva.js
@@ -0,0 +1,15 @@
+let isChuva = true;
+
+if(isChuva == true){
+    console.log("Está chovendo");
+} else {
+    console.log("Não está chovendo");
+};
+
+isChuva = false;
+
+if(isChuva == true){
+    console.log("Está chovendo");
+} else {
+    console.log("Não está chovendo");
+};
