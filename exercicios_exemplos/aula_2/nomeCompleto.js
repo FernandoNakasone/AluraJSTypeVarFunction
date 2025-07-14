25427e980938d17e8a815c1b1c36945ec9bbbb3d
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-13 22:22:19 -0300

    Exercicio template string

---
+++ exercicios_exemplos/aula_2/nomeCompleto.js
@@ -0,0 +1,10 @@
+let primeiroNome = 'Fernando';
+let sobrenome = 'Nakasone';
+
+let nomeMais = primeiroNome  + "  " + sobrenome
+let nomeTemplate = `${primeroNome} ${sobrenome}`;
+
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log(nomeMais);
+console.log(nomeTemplate);
