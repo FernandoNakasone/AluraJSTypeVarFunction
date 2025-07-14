ae9c5bb5e982412957a236cda88f6556304ef0b1
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-13 22:38:47 -0300

    Exercicio var let

---
+++ exercicios_exemplos/aula_2/varLet.js
@@ -0,0 +1,17 @@
+var nome = 'Fernando';
+let nome2 = 'Fernando';
+
+console.log(nome);
+console.log(nome2);
+
+if(true){
+    var sobrenome = 'Nakasone';
+    let sobrenome2 = 'Nakasone';
+    console.log(nome);
+    console.log(nome2);
+    console.log(sobrenome);
+    console.log(sobrenome2);
+};
+
+console.log(sobrenome);
+//console.log(sobrenome2); como a variavel sobrenome2 está dentro do IF ela não tem valor atribuido nela fora do IF
