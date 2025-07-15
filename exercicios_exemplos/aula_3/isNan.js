c60f9ae34dc55150aa09e4ce9849283c71886732
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-14 22:32:34 -0300

    exemplos isNan e tipo de números

---
+++ exercicios_exemplos/aula_3/isNan.js
@@ -0,0 +1,6 @@
+const numero = 10;
+const nome = 'Ana';
+
+Number.isNaN(numero) // false
+Number.isNaN(nome) // false
+Number.isNaN(NaN) // true
---
+++ exercicios_exemplos/aula_3/numeros.js
@@ -0,0 +1,15 @@
+//Número inteiro
+let numeroInteiro = 10;
+console.log(numeroInteiro); 
+
+//Número Flutuante (float)
+let numeroFlutuante = 3.14;
+console.log(numeroFlutuante); 
+
+//Número Hexadecimal
+let numHex = 0xA; 
+console.log(numHex); 
+
+//Número Octais
+let numOctal = 0o10;
+console.log(numOctal);
