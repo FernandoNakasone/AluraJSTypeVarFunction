106b12249847fb05962680ef1fb09753f6ea7ee1
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-15 23:10:48 -0300

    tipos de string

---
+++ exercicios_exemplos/aula_3/tipo-string.js
@@ -0,0 +1,12 @@
+const estudante = "Fernando";
+const docente = "Anna";
+const cumprimento = "Nosso lema é 'estudar bastante!'";
+const citacao = `Ju dis: ${cumprimento}`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`a estudante chama ${estudante}`);
+
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
