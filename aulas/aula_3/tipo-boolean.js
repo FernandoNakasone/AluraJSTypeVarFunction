aba5eabcc82be23023baa18f51bee0315428314f
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-16 23:37:52 -0300

    Exempo tipo boolean

---
+++ aulas/aula_3/tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if(estaAprovado){
+    console.log(`Parabéns ${estudante}, boas festas`);
+} else {
+     console.log(`Infelizmente ${estudante}, você foi reprovado`);
+}
+
+if(estudante === 'Fernando'){
+    console.log(`Olá, ${estudante}`);
+} else {
+    console.log('Quem é você?');
+}
