c87447a9434587acf99ec610ac8ca4399db45996
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-17 22:44:29 -0300

    Exercicios strings template string e valor null

---
+++ exercicios_exemplos/aula_3/ex_strings.js
@@ -0,0 +1,6 @@
+const nome = 'Fernando';
+
+console.log(nome);
+console.log(nome.toLowerCase());
+console.log(nome.toUpperCase());
+console.log(nome.length);
---
+++ exercicios_exemplos/aula_3/ex_template-string.js
@@ -0,0 +1,5 @@
+const nome = 'Fernando';
+const idade = 20;
+const estuda = true;
+
+console.log(`olá meu nome é ${nome} tenho ${idade}, estou gostando do curso, ${estuda}`);
