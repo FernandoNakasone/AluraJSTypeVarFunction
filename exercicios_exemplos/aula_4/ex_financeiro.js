f7d1dd746dd236eefc5b3861f63fed376f27d9a4
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-18 19:02:22 -0300

    Ex financeiro e contador e exemplo de precedencia

---
+++ exercicios_exemplos/aula_4/ex_financeiro.js
@@ -0,0 +1,10 @@
+const salarioMensal = 3500; 
+const despesasFixas = 1200; 
+const despesasVariaveis = 500; 
+const economiasMensais = 800;
+const bonusAnual = 3000;
+
+const resultado = (salarioMensal - despesasFixas - despesasVariaveis) * 12 + (economiasMensais * 12) + bonusAnual;
+
+//Qual é o valor de resultado após a execução da expressão?
+console.log(resultado.toFixed(2));
