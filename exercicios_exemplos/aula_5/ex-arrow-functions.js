1df0b4ed81d8161e2328f78baf7aeeb9e82dd009
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-24 23:41:33 -0300

    exercicio arrow function

---
+++ exercicios_exemplos/aula_5/ex-arrow-functions.js
@@ -0,0 +1,30 @@
+function calculadoraSimples(a, b, operacao) {
+    let resultado;
+    if (operacao === 'soma') {
+        resultado = a + b;
+    } else if (operacao === 'subtracao') {
+        resultado = a - b;
+    } else if (operacao === 'multiplicacao') {
+        resultado = a * b;
+    } else if (operacao === 'divisao') {
+        resultado = a / b;
+    } else {
+        resultado = 'Operação não reconhecida';
+    }
+    return resultado;
+}
+
+//arrow function
+const calculadoraSimples = (a, b, operacao) => {
+    if (operacao === 'soma') {
+        return a + b;
+    } else if (operacao === 'subtracao') {
+        return a - b;
+    } else if (operacao === 'multiplicacao') {
+        return a * b;
+    } else if (operacao === 'divisao') {
+        return a / b;
+    } else {
+        return 'Operação não reconhecida';
+    }
+};
