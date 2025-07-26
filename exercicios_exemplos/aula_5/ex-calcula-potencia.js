95ed116814bf48bd3f8578a56d6fe81180d16aec
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-25 21:36:27 -0300

    exercicio arrow function calcular potencia)

---
+++ exercicios_exemplos/aula_5/ex-calcula-potencia.js
@@ -0,0 +1,8 @@
+const resultado = (base,expoente) => {
+    return Math.pow(base,expoente);
+}
+
+console.log(resultado(10,5));
+console.log(resultado(10,10));
+console.log(resultado(5,10));
+console.log(resultado(3,3));
