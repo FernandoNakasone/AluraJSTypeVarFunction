87a52020a67c8b9666ce38a01a2bd3cb431e62ac
FernandoNakasone <fernandonakasone040@gmail.com>
2025-07-25 21:18:26 -0300

    ex verificar maioridade

---
+++ exercicios_exemplos/aula_5/ex-idade.js
@@ -0,0 +1,11 @@
+const idade = 20;
+
+function verificarIdade(idade){
+    if(idade >= 18){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verificarIdade(idade));
